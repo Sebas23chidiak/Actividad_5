3e37a61c54bb6e85fc02015ccdfe3dfb8a3c9a2c
Sebas23chidiak <143117455+Sebas23chidiak@users.noreply.github.com>
2023-11-01 17:27:57 -0500

    Add files via upload

---
+++ Ejercicio 8.3/src/main/java/com/mycompany/calculadorageometrica/CalculadoraGeometrica.java
@@ -0,0 +1,11 @@
+package com.mycompany.calculadorageometrica;
+
+public class CalculadoraGeometrica {
+
+    public static void main(String[] args) {
+       VentanaPrincipal miVentanaPrincipal; 
+       miVentanaPrincipal= new VentanaPrincipal();
+       miVentanaPrincipal.setVisible(true);
+       miVentanaPrincipal.setResizable(false);
+    }
+}
